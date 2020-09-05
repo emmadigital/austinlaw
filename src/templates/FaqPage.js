6af8992bdeb65ca9de87513f6401af1eb9ec0a1d
emmadigital <emma.digital.official@gmail.com>
2020-09-06 01:43:25 +0300

    update pagination

--- src/templates/FaqPage.js
+++ src/templates/FaqPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import _get from 'lodash/get'
+import { Link, graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
 import Content from '../components/Content.js'
 import Layout from '../components/Layout.js'
+import './SinglePost.css'
 
 // Export Template for use in CMS preview
 export const FaqPageTemplate = ({
@@ -11,6 +13,10 @@ export const FaqPageTemplate = ({
   excerpt,
   featuredImage,
   body,
+  nextPostURL,
+  prevPostURL,
+  nextPostTitle,
+  prevPostTitle,
 }) => (
   <main>
     <PageHeader
@@ -21,19 +27,50 @@ export const FaqPageTemplate = ({
     <section className="section">
       <div className="container">
         <Content source={body} />
+        <div  style={{margin: '4rem 0 0 0', display: 'flex'}}>
+            {prevPostURL && (
+              <Link
+                className="SinglePost--Pagination--Link"                
+                to={prevPostURL}
+              >
+                {prevPostTitle}
+              </Link>
+            )}
+            {nextPostURL && (
+              <Link
+                className="SinglePost--Pagination--Link next"
+               
+                to={nextPostURL}
+              >
+                {nextPostTitle}
+              </Link>
+            )}
+          </div>
+
       </div>
     </section>
   </main>
 )
 
-const FaqPage = ({ data: { page } }) => (
+const FaqPage = ({ data: { page, allPosts } }) => {
+  const thisEdge = allPosts.edges.find(edge => edge.node.id === page.id)  
+return (
   <Layout
     meta={page.frontmatter.meta || false}
     title={page.frontmatter.title || false}
   >
-    <FaqPageTemplate {...page} {...page.frontmatter} body={page.html} />
+    <FaqPageTemplate 
+    {...page} 
+    {...page.frontmatter} 
+    body={page.html} 
+    nextPostURL={_get(thisEdge, 'next.fields.slug')}
+    prevPostURL={_get(thisEdge, 'previous.fields.slug')}
+    nextPostTitle={_get(thisEdge, 'next.frontmatter.title')}
+    prevPostTitle={_get(thisEdge, 'previous.frontmatter.title')}    
+    />
   </Layout>
 )
+  }
 
 export default FaqPage
 
@@ -42,12 +79,42 @@ export const pageQuery = graphql`
     page: markdownRemark(id: { eq: $id }) {
       ...Meta
         html
+        id
       frontmatter {
         title
         template
+        date(formatString: "MMMM Do, YYYY")
         featuredImage
         excerpt
       }
     }
+    allPosts: allMarkdownRemark(
+      filter: { fields: { contentType: { eq: "faq" } } }
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          id
+        }
+        next {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+        previous {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            
+          }
+        }
+      }
+    } 
+
   }
 `
